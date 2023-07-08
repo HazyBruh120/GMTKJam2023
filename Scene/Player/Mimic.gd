a0e7b62578749f8aa0437ac1d26119e072d1cf0e
HazyBruh120 <stormlakes@gmail.com>
2023-07-07 21:59:06 -0400

    Moving mimic

--- Scene/Player/Mimic.gd
+++ Scene/Player/Mimic.gd
@@ -6,13 +6,19 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
 
+func _ready():
+	animationTree.active = true
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_vector = Vector2(Input.get_axis("ui_left", "ui_right"),Input.get_axis("ui_up", "ui_down"))
-	if input_vector:
+	animationTree.set("parameters/Idle/blend_position",input_vector)
+	animationTree.set("parameters/Run/blend_position",input_vector)
+	if input_vector != Vector2.ZERO:
 		velocity = input_vector * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
