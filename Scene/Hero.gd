8fd32e2e4a8446da4ccd0d9e375e33e2beebc364
Peyton Williams <peytonwilliams5@gmail.com>
2023-07-07 22:33:17 -0400

    delete loot on pickup

--- Scene/Hero.gd
+++ Scene/Hero.gd
@@ -1,41 +1,41 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 150.0
 
 @onready var scanner = $DetectionArea
 @onready var nav_agent = $NavigationAgent2D
+
+var target_loot: Area2D = null
 
 func _init():
 	pass
 
 func _physics_process(delta):
-	if scanner.has_overlapping_areas():
-		var nearby_loot = scanner.get_overlapping_areas()
 
-		var target_loot: Area2D = nearby_loot[0]
-		for loot in nearby_loot:
-			if position.distance_to(loot.position) < position.distance_to(target_loot.position):
-				target_loot = loot
-
+	if target_loot != null:
 		nav_agent.target_position = target_loot.global_position
 		var next_pos: Vector2 = nav_agent.get_next_path_position()
 
 		velocity = (next_pos - position).normalized() * SPEED
-		print("yes", velocity)
+
+	elif scanner.has_overlapping_areas():
+		var nearby_loot = scanner.get_overlapping_areas()
+
+		target_loot = nearby_loot[0]
+		for loot in nearby_loot:
+			if position.distance_to(loot.position) < position.distance_to(target_loot.position):
+				target_loot = loot
 
 	else:
-		velocity.move_toward(Vector2.ZERO, SPEED)
-		print("no", velocity)
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 
 	move_and_slide()
 
 
 
-	pass
+func _on_interact_area_area_entered(area):
+	var area_parent = area.get_parent()
+	if area_parent.is_in_group("loot"):
+		area_parent.queue_free()
+		target_loot = null
 
-
-
-#	velocity.x = direction * SPEED
-#	velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-#	move_and_slide()
