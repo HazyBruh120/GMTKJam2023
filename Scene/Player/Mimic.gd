6b78c1f2ab7383edb19903878391e366f08fa5c9
HazyBruh120 <stormlakes@gmail.com>
2023-07-08 17:32:30 -0400

    Modify Mimic

--- Scene/Player/Mimic.gd
+++ Scene/Player/Mimic.gd
@@ -7,33 +7,37 @@ const SPEED = 150.0
 @onready var animationState = animationTree["parameters/playback"]
 @onready var particles = $CPUParticles2D
 @onready var stealthTimer:Timer = $StealthTimer
+@onready var qteSlider = $qteSlider
+@onready var valSlider = $valueSlider
+@onready var qteTimer = $qteTimer
+@onready var delayTimer = $delayTimer
 
 var boostMeter:float = 1
+var hungerMeter:float = 1
 var is_hidden:bool = false
-var quickTimer:Timer
+var qte = {
+	"timer": Timer.new(),
+	"delayTimer": Timer.new(),
+	"window": 1.0,
+	"delay": 2.0,
+	"range": 0.2,
+	"wantedTime": randf_range(0.1,0.9), ## Randomized every qte
+	"success": false ## for use in the process
+	} : set = _set_qte
 
 func _ready():
 	animationTree.active = true
-	quickTimer = Timer.new()
-	quickTimer.autostart=true
-	quickTimer.wait_time = 1
-	add_child(quickTimer)
+	qte["timer"].wait_time = qte["window"]
+	qte["timer"].wait_time = qte["delay"]
+	add_child(qte["timer"])
+	add_child(qte["delayTimer"])
 
 func _process(delta):
+	
+	process_qte()
+	
 	var input_vector = velocity.normalized()
-
-	modulate = Color.CRIMSON if is_hidden else  Color.WHITE
-
-	var wantedTime = randf_range(0.1,0.9)
-	var range = 0.2
-	print(quickTimer.time_left)
-	if wantedTime < quickTimer.time_left-range and wantedTime > quickTimer.time_left+range :
-		is_hidden = false
-	if is_hidden and quickTimer.is_stopped() :
-		quickTimer.start()
-	else:
-		quickTimer.stop()
-
+	
 	if input_vector != Vector2.ZERO :
 		animationTree.set("parameters/Idle/blend_position",input_vector)
 		animationTree.set("parameters/Move/blend_position",input_vector)
@@ -47,7 +51,6 @@ func _process(delta):
 	else :
 		if !is_hidden and stealthTimer.is_stopped():
 			stealthTimer.start()
-
 	#	$LureArea.monitorable = Input.is_action_pressed("lure")
 		$LureArea.visible = Input.is_action_pressed("lure")
 
@@ -72,5 +75,51 @@ func _physics_process(delta):
 	move_and_slide()
 
 
+func _set_qte(new_qte):
+	qte["timer"].wait_time = new_qte["window"] + new_qte["delay"]
+	qte["window"] = new_qte["window"]
+	qte["delay"] = new_qte["delay"]
+
+
+func set_quickTime_event(window:float,delay:float):
+	qte["timer"].wait_time = window + delay
+	qte["window"] = window
+	qte["delay"] = delay
+	
+func process_qte():
+	if (qte["wantedTime"] < qte["timer"].time_left-qte["range"] or qte["wantedTime"] > qte["timer"].time_left+qte["range"]) and \
+		Input.is_action_pressed("qte") and \
+		qte["timer"].time_left <= qte["window"]:
+		is_hidden = false
+		qte["success"] = false
+	elif (qte["wantedTime"] > qte["timer"].time_left-qte["range"] and qte["wantedTime"] < qte["timer"].time_left+qte["range"]) and \
+		Input.is_action_pressed("qte") and \
+		qte["timer"].time_left <= qte["window"]:
+		qte["success"] = true
+	
+	if is_hidden and qte["timer"].is_stopped() :
+		qteSlider.visible = true
+		valSlider.visible = true
+		qte["timer"].start()
+	elif !is_hidden :
+		qteSlider.visible = false
+		valSlider.visible = false
+		qte["timer"].stop()
+	elif qte["timer"].time_left <= 0.1 :
+		qte["wantedTime"] = randf_range(0.1,qte["window"]-0.1)
+		valSlider.value = qte["wantedTime"]*100
+		is_hidden = qte["success"]
+	
+	qteSlider.value = qte["timer"].time_left*100
+
+
 func _on_stealth_timer_timeout():
 	is_hidden = true
+
+
+func _on_delay_timer_timeout():
+	qte["success"] = false
+
+
+func _on_qte_timer_timeout():
+	pass # Replace with function body.
