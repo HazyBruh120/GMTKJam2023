2df3f28ad951a81166b916597694d5ee027a6c4a
HazyBruh120 <stormlakes@gmail.com>
2023-07-08 14:01:54 -0400

    Adding is_hidden property

--- Scene/Player/Mimic.gd
+++ Scene/Player/Mimic.gd
@@ -6,25 +6,30 @@ const SPEED = 150.0
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree["parameters/playback"]
 @onready var particles = $CPUParticles2D
-@onready var timer:Timer = $StealthTimer
+@onready var stealthTimer:Timer = $StealthTimer
 
 var boostMeter:float = 1
+var is_hidden:bool = false
 
 func _ready():
 	animationTree.active = true
 
 func _process(delta):
-	if animationState.get_current_node() == "Idle" :
-	#	Stealth mechanic for 
+	var input_vector = velocity.normalized()
 	
+	modulate = Color.CRIMSON if is_hidden else  Color.WHITE
+	
+	if animationState.get_current_node() == "Idle":
+		if !is_hidden and stealthTimer.is_stopped():
+			stealthTimer.start()
 	#	$LureArea.monitorable = Input.is_action_pressed("lure")
 		$LureArea.visible = Input.is_action_pressed("lure")
-
-	var input_vector = velocity.normalized()
+		
 	if input_vector != Vector2.ZERO :
 		animationTree.set("parameters/Idle/blend_position",input_vector)
 		animationTree.set("parameters/Move/blend_position",input_vector)
 		particles.gravity = particles.gravity.rotated(particles.gravity.angle_to(-input_vector))
+		is_hidden = false
 		if animationState.get_current_node() != "Move" :
 			animationState.travel("Move")
 	elif animationState.get_current_node() != "Idle" :
@@ -42,10 +47,14 @@ func _physics_process(delta):
 			boostMeter += delta
 	else:
 		boostMeter = 1
-
+	
 	if input_vector != Vector2.ZERO:
 		velocity = input_vector * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	
 	move_and_slide()
+
+
+func _on_stealth_timer_timeout():
+	is_hidden = true
