68d4f70140b94a440fb81515af5b531e23019a5c
Peyton Williams <peytonwilliams5@gmail.com>
2023-07-09 10:30:58 -0400

    menu work

--- Scene/Shipping Levels/LevelLogic.gd
+++ Scene/Shipping Levels/LevelLogic.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-signal win
+signal win(state)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Mimic.die.connect(on_mimic_die)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if $Mobs.get_child_count() == 0:
-		win.emit()
+		win.emit(true)
+
+
+func on_mimic_die():
+	win.emit(false)
+
+
+
--- Scene/Shipping Levels/main.gd
+++ Scene/Shipping Levels/main.gd
@@ -1,9 +1,22 @@
 extends Node
 
-const TUTLVL1 = preload("res://Scene/Shipping Levels/Tutorial Level 1.tscn")
 const TUTLVL2 = preload("res://Scene/Shipping Levels/Tutorial Level 2.tscn")
+const GLVL1 = preload("res://Scene/Shipping Levels/Game Level 1.tscn")
+
+const LVL_ORDERING_DICT = {
+	"Tutorial Level 1": {
+		"pack": preload("res://Scene/Shipping Levels/Tutorial Level 1.tscn"),
+		"next": "Tutorial Level 2"
+	},
+	"Tutorial Level 2": {
+		"pack": preload("res://Scene/Shipping Levels/Tutorial Level 2.tscn"),
+		"next": "Game Level 1"
+	}
+
+}
 
 var curr_level: Node2D = null
+var last_level_name = ""
 
 var state = "menu"
 
@@ -33,25 +46,35 @@ func process_state_change(new_state):
 		$Music/WinMusic.play()
 
 
-func handle_win():
-	if curr_level.name == "Tutorial Level 1":
-		swap_level(TUTLVL2)
-		print(curr_level.name)
-	if curr_level.name == "pass":
-		pass
+func handle_win(state):
+	if state:
+		$NextLevel.show()
+	else:
+		$RestartLevel.show()
 
 
-func swap_level(new_level):
+func unload_level():
+	last_level_name = curr_level.name
 	remove_child(curr_level)
 	curr_level.queue_free()
-	curr_level = new_level.instantiate()
+
+func next_level():
+	var next_level_name = LVL_ORDERING_DICT[last_level_name]["next"]
+
+	curr_level = LVL_ORDERING_DICT[next_level_name]["pack"].instantiate()
+	add_child(curr_level)
+	curr_level.win.connect(handle_win)
+
+
+func restart_level():
+	curr_level = LVL_ORDERING_DICT[last_level_name]["pack"].instantiate()
 	add_child(curr_level)
 	curr_level.win.connect(handle_win)
 
 
 
 func _on_start_pressed():
-	curr_level = TUTLVL1.instantiate()
+	curr_level = LVL_ORDERING_DICT["Tutorial Level 1"]["pack"].instantiate()
 	add_child(curr_level)
 	process_state_change("game")
 	curr_level.win.connect(handle_win)
@@ -59,3 +82,13 @@ func _on_start_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_next_pressed():
+	$NextLevel.hide()
+	next_level()
+
+
+func _on_restart_pressed():
+	$RestartLevel.hide()
+	restart_level()
